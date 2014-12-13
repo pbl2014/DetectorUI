be057f95993de45d11242f79998f08e0fd15de68
kaku13745107 <a1307rh@aiit.ac.jp>
2014-12-13 16:57:23 +0900

    choose folder sample push by kaku

--- src/jp/ac/aiit/Detector/Controller.java
+++ src/jp/ac/aiit/Detector/Controller.java
@@ -2,13 +2,33 @@ package jp.ac.aiit.Detector;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Controller {
 
     @FXML
+    private Label dirlabel;
+
+    @FXML
     private void dirButtonAction(ActionEvent event) {
         // Button was clicked, do something...
-        //outputTextArea.appendText("Button Action\n");
-        System.out.println("test button action");
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Please choose the path for target pictures!");
+
+        //Show open file dialog
+        File file = directoryChooser.showDialog(null);
+
+        if(file!=null){
+            //labelFile.setText(file.getPath());
+            System.out.println(file.getPath());
+            dirlabel.setText(file.getPath());
+        }
+
     }
+
+
 }
