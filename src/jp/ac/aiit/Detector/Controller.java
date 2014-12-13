cf168b0fe3884e57170ed87578933a6823b34f43
kaku13745107 <a1307rh@aiit.ac.jp>
2014-12-13 15:02:01 +0900

    project init by kaku

---
+++ src/jp/ac/aiit/Detector/Controller.java
@@ -0,0 +1,14 @@
+package jp.ac.aiit.Detector;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private void dirButtonAction(ActionEvent event) {
+        // Button was clicked, do something...
+        //outputTextArea.appendText("Button Action\n");
+        System.out.println("test button action");
+    }
+}
